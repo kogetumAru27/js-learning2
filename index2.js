d16cc911ba7f5e9581e370630d906bb02b2e9b1d
kogetumAru27 <tm10tora@icloud.com>
2026-02-17 22:32:19 +0900

    filter practice

---
+++ index2.js
@@ -0,0 +1,3 @@
+const number = [1,2,3,4,5];
+const NewNub =number.filter(n => n % 2 === 0 );
+console.log(NewNub);
---
+++ index_prc.js
@@ -0,0 +1,3 @@
+const number = [10,15,20,30,35];
+const NewNub = number.filter(n => n >= 20);
+console.log(NewNub);
